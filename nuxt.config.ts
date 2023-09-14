c3418197eb954e237a0f4db436bab6251011eaf7
rkolesnikov <roman_kolesnikov@govitall.com>
2023-09-14 17:50:09 +0300

    feat: update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { NuxtConfig } from "@nuxt/types";
-
-const config: NuxtConfig = {
+export default defineNuxtConfig({
+  css: ["~/assets/scss/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/scss/core/_variables.scss"; @import "@/assets/scss/utils/_mixins.scss";`,
+        },
+      },
+    },
+  },
+  imports: {
+    dirs: ["store"],
+  },
+  modules: ["@pinia/nuxt"],
+  pinia: {
+    autoImports: ["defineStore", "storeToRefs"],
+  },
   devtools: { enabled: true },
-  buildModules: ["@nuxt/typescript-build"],
-};
-export default config;
+});
