e1eafee9bdb78a52df3849fe735288ff28fcd325
rkolesnikov <roman_kolesnikov@govitall.com>
2023-09-16 12:29:08 +0300

    feat: add markdown components

---
+++ components/EditorComponent.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+const { saveNote } = useStore();
+const { rawText } = storeToRefs(useStore());
+</script>
+
+<template>
+  <div class="editor d-flex flex-column">
+    <textarea
+      v-model="rawText"
+      class="editor__area"
+      @input="saveNote()"
+    ></textarea>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.editor {
+  overflow: auto;
+  height: 100%;
+
+  &__area {
+    flex: 1;
+    padding: 0.9rem 1.6rem;
+    background-color: var(--bg-color);
+    color: var(--header-text-color);
+    overflow: auto;
+
+    border: none;
+    outline: none;
+    resize: none;
+
+    @include font(1.4rem, 1.6rem);
+  }
+}
+</style>
---
+++ components/PreviewComponent.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+import { Remarkable } from "remarkable";
+
+const { rawText, parsedText, isPreviewActive } = storeToRefs(useStore());
+
+const md = new Remarkable({
+  html: true,
+  breaks: true,
+  linkify: true,
+  typographer: true,
+});
+
+const parseMarkdown = (text: string) => {
+  return md.render(text);
+};
+
+watchEffect(() => {
+  parsedText.value = parseMarkdown(rawText.value);
+});
+</script>
+
+<template>
+  <div class="preview d-flex flex-column">
+    <div
+      class="preview__area"
+      :class="{ active: isPreviewActive }"
+      v-html="parsedText"
+    ></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.preview {
+  overflow: auto;
+
+  &__area {
+    flex: 1;
+    padding: 0.9rem 1.6rem;
+    background-color: var(--bg-color);
+    color: var(--header-text-color);
+    overflow: auto;
+    padding-bottom: 10rem;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  .active {
+    width: 100%;
+    max-width: 70rem;
+    margin: 0 auto;
+  }
+}
+</style>
