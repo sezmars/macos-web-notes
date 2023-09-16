4c4c2ba10e6d0aaa5ab2195a420fdbf692f5f7b7
rkolesnikov <roman_kolesnikov@govitall.com>
2023-09-16 12:29:18 +0300

    feat: add ui components

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,109 @@
+<script lang="ts" setup>
+const { currentNoteUpdateDate, isPreviewActive } = storeToRefs(useStore());
+
+interface Emits {
+  (event: Common.toggleMenu, value: boolean): void;
+  (event: Common.toggleDeleteModal, value: boolean): void;
+}
+const emits = defineEmits<Emits>();
+const props = defineProps({
+  dateState: Boolean,
+});
+const isMenuOpen = ref(false);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+  emits(Common.toggleMenu, isMenuOpen.value);
+};
+</script>
+
+<template>
+  <header class="w-100 d-flex align-items-center">
+    <div class="left d-flex align-items-center gap-5">
+      <div class="header position-sticky d-flex align-items-center weight-500">
+        <IconsMarkdown
+          v-if="isPreviewActive"
+          class="cursor-pointer"
+          :is-preview-active="isPreviewActive"
+          @click="isPreviewActive = !isPreviewActive"
+        />
+
+        <IconsEdit
+          v-else
+          class="cursor-pointer"
+          :is-preview-active="isPreviewActive"
+          @click="isPreviewActive = !isPreviewActive"
+        />
+      </div>
+    </div>
+
+    <div class="right d-flex align-items-center gap-9">
+      <SearchComponent />
+    </div>
+  </header>
+  <div
+    v-if="props.dateState && currentNoteUpdateDate"
+    class="d-flex justify-content-center date"
+  >
+    {{ formatDate(currentNoteUpdateDate, "fullDate") }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+header {
+  height: 5rem;
+  padding: 2rem;
+  justify-content: space-between;
+
+  @media (max-width: 480px) {
+    padding: 1rem;
+  }
+
+  .left {
+    .menu {
+      height: 7.2rem;
+      width: 12rem;
+      background: $col-darkGrayishBlue;
+      color: $col-white;
+      transition: background 0.2s ease-in-out;
+
+      &:hover {
+        background: $col-redOrange;
+      }
+
+      @media (max-width: 550px) {
+        width: 7.2rem;
+      }
+    }
+
+    .logo {
+      max-width: 12rem;
+      margin-left: 2.4rem;
+
+      @media (max-width: 768px) {
+        display: none;
+      }
+    }
+
+    hr {
+      height: 4rem;
+      width: 1px;
+      background: $col-lightCyanBlue;
+      border: none;
+      margin: 0 2.4rem;
+
+      @media (max-width: 768px) {
+        display: none;
+      }
+    }
+  }
+
+  .right {
+    padding-right: 1.5rem;
+  }
+}
+.date {
+  color: $col-lightCyanBlue;
+  @include font(1.3rem, 1.5rem);
+}
+</style>
---
+++ components/SearchComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="input-icon-wrap">
+    <span class="input-icon"><IconsSearch /></span>
+    <input placeholder="Search" type="text" class="input-with-icon" />
+  </div>
+</template>
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.input-icon-wrap {
+  border: 1px solid $col-lightBlueShade;
+  display: flex;
+  flex-direction: row;
+  border-radius: 8px;
+}
+
+.input-with-icon {
+  border: none;
+  flex: 1;
+}
+
+.input-with-icon {
+  padding: 4px;
+  border-radius: 8px;
+}
+
+.input-icon {
+  padding: 4px 8px 4px 8px;
+  border-radius: 8px;
+}
+</style>
