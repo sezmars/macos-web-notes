521cc99ba8c5f8a0399df9a9370b254c0a41807b
rkolesnikov <roman_kolesnikov@govitall.com>
2023-09-16 16:57:03 +0300

    refactor: remove idb deps

--- composables/useIdb.ts
+++ composables/useIdb.ts
@@ -1,57 +1,122 @@
-import { openDB } from 'idb'
-
 export const useMdNotes = () => {
-  const dbPromise = openDB('mdNotes', 1, {
-    upgrade (db) {
+  const dbPromise: Promise<IDBDatabase> = new Promise((resolve, reject) => {
+    const request: IDBOpenDBRequest = indexedDB.open('mdNotes', 1)
+
+    request.onupgradeneeded = (event: IDBVersionChangeEvent) => {
+      const db: IDBDatabase = (event.target as IDBOpenDBRequest)?.result
       db.createObjectStore('mdNotes', { keyPath: 'id' })
+    }
+
+    request.onsuccess = (event: Event) => {
+      resolve((event.target as IDBOpenDBRequest).result as IDBDatabase)
+    }
+
+    request.onerror = (event: Event) => {
+      reject((event.target as IDBOpenDBRequest).error)
     }
   })
 
-  const getNotes = async () => {
-    const db = await dbPromise
-    const tx = db.transaction('mdNotes', 'readonly')
-    const store = tx.objectStore('mdNotes')
+  const getNotes = async (): Promise<Note[]> => {
+    const db: IDBDatabase = await dbPromise
+    const tx: IDBTransaction = db.transaction('mdNotes', 'readonly')
+    const store: IDBObjectStore = tx.objectStore('mdNotes')
 
-    const data = await store.getAll()
-    const sortedNotes = () => {
-      if (data.length) {
-        data.sort(
-          (a: Note, b: Note) =>
-            new Date(b.updated).getTime() - new Date(a.updated).getTime()
-        )
-        return data
+    return new Promise<Note[]>((resolve, reject) => {
+      const request: IDBRequest = store.getAll()
+
+      request.onsuccess = (event: Event) => {
+        const data: Note[] = (event.target as IDBRequest).result as Note[]
+        const sortedNotes = () => {
+          if (data.length) {
+            data.sort(
+              (a: Note, b: Note) =>
+                new Date(b.updated).getTime() - new Date(a.updated).getTime()
+            )
+            resolve(data)
+          } else {
+            resolve(data)
+          }
+        }
+
+        sortedNotes()
       }
-    }
 
-    return (sortedNotes()) || (await store.getAll())
+      request.onerror = (event: Event) => {
+        reject((event.target as IDBRequest).error)
+      }
+    })
   }
 
-  const getNote = async (id: string) => {
-    const db = await dbPromise
-    const tx = db.transaction('mdNotes', 'readonly')
-    const store = tx.objectStore('mdNotes')
-    return await store.get(id)
+  const getNote = async (id: string): Promise<Note | undefined> => {
+    const db: IDBDatabase = await dbPromise
+    const tx: IDBTransaction = db.transaction('mdNotes', 'readonly')
+    const store: IDBObjectStore = tx.objectStore('mdNotes')
+
+    return new Promise<Note | undefined>((resolve, reject) => {
+      const request: IDBRequest = store.get(id)
+
+      request.onsuccess = (event: Event) => {
+        resolve((event.target as IDBRequest).result as Note)
+      }
+
+      request.onerror = (event: Event) => {
+        reject((event.target as IDBRequest).error)
+      }
+    })
   }
 
-  const saveNote = async (note: Note) => {
-    const db = await dbPromise
-    const tx = db.transaction('mdNotes', 'readwrite')
-    const store = tx.objectStore('mdNotes')
-    await store.put(note)
+  const saveNote = async (note: Note): Promise<void> => {
+    const db: IDBDatabase = await dbPromise
+    const tx: IDBTransaction = db.transaction('mdNotes', 'readwrite')
+    const store: IDBObjectStore = tx.objectStore('mdNotes')
+
+    return new Promise<void>((resolve, reject) => {
+      const request: IDBRequest = store.put(note)
+
+      request.onsuccess = () => {
+        resolve()
+      }
+
+      request.onerror = (event: Event) => {
+        reject((event.target as IDBRequest).error)
+      }
+    })
   }
 
-  const addNote = async (note: Note) => {
-    const db = await dbPromise
-    const tx = db.transaction('mdNotes', 'readwrite')
-    const store = tx.objectStore('mdNotes')
-    await store.add(note)
+  const addNote = async (note: Note): Promise<void> => {
+    const db: IDBDatabase = await dbPromise
+    const tx: IDBTransaction = db.transaction('mdNotes', 'readwrite')
+    const store: IDBObjectStore = tx.objectStore('mdNotes')
+
+    return new Promise<void>((resolve, reject) => {
+      const request: IDBRequest = store.add(note)
+
+      request.onsuccess = () => {
+        resolve()
+      }
+
+      request.onerror = (event: Event) => {
+        reject((event.target as IDBRequest).error)
+      }
+    })
   }
 
-  const deleteNote = async (id: string) => {
-    const db = await dbPromise
-    const tx = db.transaction('mdNotes', 'readwrite')
-    const store = tx.objectStore('mdNotes')
-    await store.delete(id)
+  const deleteNote = async (id: string): Promise<void> => {
+    const db: IDBDatabase = await dbPromise
+    const tx: IDBTransaction = db.transaction('mdNotes', 'readwrite')
+    const store: IDBObjectStore = tx.objectStore('mdNotes')
+
+    return new Promise<void>((resolve, reject) => {
+      const request: IDBRequest = store.delete(id)
+
+      request.onsuccess = () => {
+        resolve()
+      }
+
+      request.onerror = (event: Event) => {
+        reject((event.target as IDBRequest).error)
+      }
+    })
   }
 
   return { getNotes, getNote, saveNote, addNote, deleteNote }
