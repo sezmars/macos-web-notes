8b5214abe49f19e437667d0f1227802a34ef0cf1
rkolesnikov <roman_kolesnikov@govitall.com>
2023-09-19 09:47:41 +0300

    fix: return old config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/macos-web-notes/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets' // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
   css: ['~/assets/scss/main.scss'],
   vite: {
     css: {
